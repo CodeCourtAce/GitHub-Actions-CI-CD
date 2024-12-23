c9d0ace13f79b291b7e5fcfbf6c6f3a20e32a69e
Gianna DAconti <themeanmememan@gmail.com>
2024-12-23 11:14:41 -0500

    fixed errors in code

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,14 +3,16 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
+    if (models[modelName]?.db?.db?.listCollections){
     let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
     }).toArray()
-
+  
     if (modelExists.length) {
       await db.dropCollection(collectionName);
     }
+  }
   } catch (err) {
     throw err;
   }
-}
+};
